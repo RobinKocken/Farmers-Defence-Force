fecd4c9931c6cd567407011b37dbbd8a3a80abb1
Stefanstadd <98754444+Stefanstadd@users.noreply.github.com>
2022-09-15 21:13:14 +0200

    3 VFX voor ufo + vuurwerk

---
+++ Assets/Stefan/Scripts/fpsCount.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class fpsCount : MonoBehaviour
+{
+    TextMeshProUGUI text; 
+    public float deltaTime;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        text.text = Mathf.Ceil(fps).ToString();
+    }
+}
