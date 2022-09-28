85c7ba4c254f507eed06b52907701e7c728408d3
Robin Kocken <robin.a.kocken@gmail.com>
2022-09-28 11:04:44 +0200

    Enemy Movement Prediction

--- Assets/Robin/Scripts/Turrets/BulletScript.cs
+++ Assets/Robin/Scripts/Turrets/BulletScript.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
     public float speed;
+    Vector3 prevPos;
 
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime * transform.forward);
+        prevPos = transform.position;
+
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+
+        RaycastHit[] hits = Physics.RaycastAll(new Ray(prevPos, (transform.position - prevPos).normalized), (transform.position - prevPos).magnitude);
+        
+        for(int i = 0; i < hits.Length; i++)
+        {
+            if(hits[i].transform.CompareTag("Alien"))
+            {
+                Destroy(gameObject);
+                Debug.Log("Hit");
+            }
+        }
     }
 }
--- Assets/Robin/Scripts/Turrets/SingleShotScript.cs
+++ Assets/Robin/Scripts/Turrets/SingleShotScript.cs
@@ -7,13 +7,18 @@ public class SingleShotScript : MonoBehaviour
 {
     public AlienManager manager;
 
+    public GameObject testObj;
+
     public GameObject target;
     public GameObject bullet;
+
+    Rigidbody rbTarget;
 
     [Header("Components")]
     public Transform frame;
     public Transform cannon;
     public Transform shootPoint;
+    public Transform aimingPoint;
     public Transform gearFrame;
     public Transform gearMotor;
 
@@ -22,10 +27,19 @@ public class SingleShotScript : MonoBehaviour
     public float rotGearFrame;
     public float rotGearMotor;
 
+    [Header("Other")]
     float startTime;
     public float waitForSeconds;
 
     bool aggro;
+
+
+    [Header("Prediction")]
+    public Vector3 currentPosition;
+    public Vector3 currentVelocity;
+    public Vector3 prev;
+    public float targetDistance;
+    public float travelTime;
 
     void Start()
     {
@@ -34,29 +48,51 @@ public class SingleShotScript : MonoBehaviour
 
     void Update()
     {
-        Aiming();
-
+        PredictMovement();
+        Test();
         if(Time.time - startTime > waitForSeconds)
         {
-            GameObject hello = Instantiate(bullet, shootPoint.transform.position, shootPoint.transform.rotation);
+            GameObject hello = Instantiate(bullet, shootPoint.transform.position, Quaternion.identity);
+
+            hello.transform.SetParent(shootPoint.transform);
+            hello.transform.localRotation = shootPoint.transform.localRotation;
+            hello.transform.parent = null;
+
             startTime = Time.time;
         }
 
     }
 
-    void Aiming()
+    void PredictMovement()
+    {
+        //currentVelocity = (target.transform.position - prev) / Time.deltaTime;
+        currentVelocity = new Vector3(0, 0, 1);
+        prev = target.transform.position;
+
+        currentPosition = target.transform.position;
+
+        targetDistance = Vector3.Distance(shootPoint.transform.position, target.transform.position);
+        travelTime = targetDistance / bullet.GetComponent<BulletScript>().speed;
+
+        Vector3 predictedPosition = currentPosition + currentVelocity * travelTime;
+
+        testObj.transform.position = predictedPosition;
+    }    
+
+    void Test()
     {
         if(aggro)
         {
             //Frame Rot
-            Quaternion frameQuat = Quaternion.Slerp(frame.transform.localRotation, Quaternion.LookRotation(target.transform.position - frame.transform.position), rotSpeed * Time.deltaTime);
+            Quaternion frameQuat = Quaternion.Slerp(frame.transform.localRotation, Quaternion.LookRotation(testObj.transform.position - frame.transform.position), rotSpeed * Time.deltaTime);
             frame.transform.localEulerAngles = new Vector3(0, frameQuat.eulerAngles.y, 0);
 
             //Cannon Rot
-            Quaternion cannonQuat = Quaternion.Slerp(cannon.transform.localRotation, Quaternion.LookRotation(target.transform.position - cannon.transform.position), rotSpeed * Time.deltaTime);
+            Quaternion cannonQuat = Quaternion.Slerp(cannon.transform.localRotation, Quaternion.LookRotation(testObj.transform.position - cannon.transform.position), rotSpeed * Time.deltaTime);
             cannon.transform.localEulerAngles = new Vector3(cannonQuat.eulerAngles.x, -90, 0);
 
             //Gear Frame
+            //Work in Progress
             Vector3 oldGearCannonAngles = cannon.transform.localEulerAngles;
 
             if(oldGearCannonAngles != cannon.transform.localEulerAngles)
@@ -79,6 +115,8 @@ public class SingleShotScript : MonoBehaviour
         {
             aggro = true;
             target = other.gameObject;
+
+            rbTarget = target.GetComponent<Rigidbody>();
         }
     }
 
